85a9bdfd7df92031abc42dd5c5caf2932669f2c6
antek <antonimalinowski.kontakt@gmail.com>
2022-02-08 09:38:41 +0100

    Simple collections with arrays

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(args.length);
+//        System.out.println(args[3] + args[2]);
+
+        int[] nums = {10, 20, 30, 40, 50};
+        System.out.println(nums.length);
+        System.out.println(nums[1]);
     }
 }
