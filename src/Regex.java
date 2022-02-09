f7d4f3669a8366c73ca0c26cb168dd9af7640947
antek <antonimalinowski.kontakt@gmail.com>
2022-02-09 11:51:44 +0100

    Comments in regular expressions

--- src/Regex.java
+++ src/Regex.java
@@ -3,12 +3,18 @@ import java.util.regex.Pattern;
 
 public class Regex {
     public static void main(String[] args) {
-        String regex = "((?<countryCode>\\d{1,2})[-,.\\s]?)?((?<areaCode>\\d{3})[-,.\\s]?)((?<exchange>\\d{3})[-,.\\s]?)(?<lineNumber>\\d{4})";
-        String phoneNumber = "12.346.532.5347";
+        String regex = """
+# This is my regex to parse the parts of a phone number
+((?<countryCode>\\d{1,2})[-,.\\s]?)? # Get's country code
+(\\(?(?<areaCode>\\d{3})\\)?[-,.\\s]?) # Get's area code
+((?<exchange>\\d{3})[-,.\\s]?) # Get's exchange
+(?<lineNumber>\\d{4}) # Get's line number
+""";
+        String phoneNumber = "(231) 333-2538";
 
-        System.out.println(phoneNumber.matches(regex));
+//        System.out.println(phoneNumber.matches(regex));
 
-        Pattern pat = Pattern.compile(regex);
+        Pattern pat = Pattern.compile(regex, Pattern.COMMENTS);
         Matcher mat = pat.matcher(phoneNumber);
 
         if(mat.matches()) {
