0a8a93c00b8e9501cae2c51b3f49226fd95c4998
antek <antonimalinowski.kontakt@gmail.com>
2022-02-17 00:17:52 +0100

    Number Guessing Game, For Loop Alternative

--- src/GuessIt.java
+++ src/GuessIt.java
@@ -6,23 +6,21 @@ public class GuessIt {
 
     public static void main(String[] args) {
         int randomNum = new Random().nextInt(10) + 1;
-        byte guessesCounter = 1;
         String guessedNumStr = null;
-        do {
+        byte guessesCounter;
+        for (guessesCounter = 1; guessesCounter <= MAX_ALLOWED_TRIES; guessesCounter++) {
             guessedNumStr = System.console().readLine("Please guess a number between 1 and 10 inclusively: ");
             if (guessedNumStr.matches("-?\\d{1,2}")) {
                 int guessedNum = Integer.parseInt(guessedNumStr);
                 if (guessedNum == randomNum) {
                     String tryText = guessesCounter == 1 ? "try" : "tries";
                     System.out.printf("The random number was %d. You got it in %d %s!%n", randomNum, guessesCounter, tryText);
-                    return;
+                    break;
                 } else {
                     System.out.printf("You didn't get it!%n", randomNum);
-                    guessesCounter++;
                 }
             }
-        } while (!"q".equals(guessedNumStr) && guessesCounter <= MAX_ALLOWED_TRIES);
-
+        }
         if (guessesCounter >= MAX_ALLOWED_TRIES) {
             System.out.format("You've had %d incorrect guesses. The random number was %d. Better luck next time!", guessesCounter - 1, randomNum);
         }
