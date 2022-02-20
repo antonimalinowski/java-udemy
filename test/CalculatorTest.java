6cbfd87c423e2b45905158963fc28f39456deaad
antek <antonimalinowski.kontakt@gmail.com>
2022-02-20 08:58:08 +0100

    Test edge case

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -28,4 +28,19 @@ public class CalculatorTest {
         int sum = calc.add(1, 1);
         assertEquals(2, sum);
     }
+
+    @Test
+    public void canAddMaxIntPlusOne() {
+        int sum = calc.add(Integer.MAX_VALUE, 1);
+        System.out.println(sum); // -2147483648
+//        System.out.println(0b01111111111111111111111111111111); // 2147483647
+//        System.out.println(0b01111111111111111111111111111111 + 0b1); // -2147483648
+//        System.out.println(0b10000000000000000000000000000000); // -2147483648
+        assertEquals(Integer.MAX_VALUE + 1l, sum);
+        /**
+         * 1 is no longer an integer (32 bits), but a long (64 bits).
+         * By doing so Integer.MAX_VALUE + 1l will be of type long which has much more room to "grow".
+         * Therefore there will be no carry.
+         */
+    }
 }
