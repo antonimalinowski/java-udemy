3c67c2049031f776d736b756ac723fd32cd0c643
antek <antonimalinowski.kontakt@gmail.com>
2022-02-20 08:31:26 +0100

    Introduce instance of a Calculator class, two scenarios, second test

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -1,12 +1,32 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
+
+    private final Calculator calc = new Calculator();
+
+    /**
+     * In the case of maintaining state by instance of a Calculator class
+     */
+//    private Calculator calc;
+
+//    @Before
+//    public void setUp() throws Exception {
+//        calc = new Calculator();
+//    }
+
     @Test
     public void canAddZeroPlusZero() {
-        Calculator calc = new Calculator();
         int sum = calc.add(0, 0);
         assertEquals(0, sum);
+    }
+
+    @Test
+    public void canAddOnePlusOne() {
+        Calculator calc = new Calculator();
+        int sum = calc.add(1, 1);
+        assertEquals(2, sum);
     }
 }
