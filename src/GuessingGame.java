0424cd94ab33b9aa0cccad00fc60104e3025109f
antek <antonimalinowski.kontakt@gmail.com>
2022-02-20 13:48:05 +0100

    Handle more than 4 guesses

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -7,12 +7,18 @@ public class GuessingGame {
 
     public String guess(int guessedNumber) {
         counter++;
-        if(counter == 4 && guessedNumber != getRandomNumber()) {
-            return "You didn't get it and you had four tries. Game over.";
-        }
         String tryText = counter == 1 ? "try" : "tries";
         String winningMessage = String.format("You got it in %d %s!", counter, tryText);
-        return guessedNumber == getRandomNumber() ? winningMessage : "You didn't get it";
+        String response = null;
+
+        if(counter == 4 && guessedNumber != getRandomNumber()) {
+            response = String.format("You didn't get it and you've had %d %s. Game over.", counter, tryText);
+        } else if (counter > 4) {
+            response = "Sorry, you are limited to only 4 tries. Your game is over.";
+        } else {
+            response = guessedNumber == getRandomNumber() ? winningMessage : "You didn't get it";
+        }
+        return response;
     }
 
     public int getRandomNumber() {
